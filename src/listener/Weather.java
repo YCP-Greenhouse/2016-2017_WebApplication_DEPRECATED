d0d82155a226a62f0338c7472d7316000ae77ebe
Kyle Musco <kyle@moena.us>
2017-04-05 21:20:10 -0400

    Weather Timer Changes
    Changed timer to query weather API when minutes = 0

--- src/listener/Weather.java
+++ src/listener/Weather.java
@@ -17,10 +17,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 // Get call weather API every hour and save results to database
 public class Weather implements ServletContextListener {
@@ -46,69 +43,75 @@ public class Weather implements ServletContextListener {
         TimerTask hourlyTask = new TimerTask() {
             @Override
             public void run() {
-                try {
-                    URLConnection urlc = url.openConnection();
 
-                    // Get weather data
-                    BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-                    String l = null;
-                    String response = "";
-                    while ((l=br.readLine())!=null) {
-                        response+=l;
-                    }
-                    br.close();
+                Calendar now = Calendar.getInstance();
 
-                    // Parse JSON response
-                   JSONObject obj = new JSONObject(response);
-                   double temperature = Double.parseDouble(obj.getJSONObject("main").get("temp").toString());
-                   double high = Double.parseDouble(obj.getJSONObject("main").get("temp_max").toString());
-                   double low = Double.parseDouble(obj.getJSONObject("main").get("temp_min").toString());
-
-                   // Save to database
-                    // Connect to database
-                    Connection conn = null;
+                // Only get weather when minute = 0
+                if (now.get(Calendar.MINUTE) == 0) {
                     try {
-                        conn = dbController.getConnection();
-                    } catch ( SQLException e ) {
+                        URLConnection urlc = url.openConnection();
+
+                        // Get weather data
+                        BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+                        String l = null;
+                        String response = "";
+                        while ((l = br.readLine()) != null) {
+                            response += l;
+                        }
+                        br.close();
+
+                        // Parse JSON response
+                        JSONObject obj = new JSONObject(response);
+                        double temperature = Double.parseDouble(obj.getJSONObject("main").get("temp").toString());
+                        double high = Double.parseDouble(obj.getJSONObject("main").get("temp_max").toString());
+                        double low = Double.parseDouble(obj.getJSONObject("main").get("temp_min").toString());
+
+                        // Save to database
+                        // Connect to database
+                        Connection conn = null;
+                        try {
+                            conn = dbController.getConnection();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                            return;
+                        }
+
+                        PreparedStatement ps = null;
+                        String sql = "INSERT INTO weather VALUES(?,?,?,?,?)";
+
+
+                        conn.setAutoCommit(false);
+                        ps = conn.prepareStatement(sql);
+
+                        // Get current time
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                        Date utcTime = new Date();
+                        Date date = new Date(utcTime.getTime() + TimeZone.getTimeZone("EST").getRawOffset());
+
+                        ps.setString(1, null);
+                        ps.setDouble(2, KelvinToFahrenheit(temperature));
+                        ps.setDouble(3, KelvinToFahrenheit(high));
+                        ps.setDouble(4, KelvinToFahrenheit(low));
+                        ps.setString(5, dateFormat.format(date));
+
+                        ps.executeUpdate();
+                        ps.close();
+
+                        conn.commit();
+                        conn.close();
+
+                    } catch (IOException e) {
                         e.printStackTrace();
-                        return;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-
-                    PreparedStatement ps = null;
-                    String sql = "INSERT INTO weather VALUES(?,?,?,?,?)";
-
-
-                    conn.setAutoCommit(false);
-                    ps = conn.prepareStatement(sql);
-
-                    // Get current time
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                    Date utcTime = new Date();
-                    Date date = new Date(utcTime.getTime() + TimeZone.getTimeZone("EST").getRawOffset() );
-
-                    ps.setString(1, null );
-                    ps.setDouble(2, KelvinToFahrenheit(temperature) );
-                    ps.setDouble(3, KelvinToFahrenheit(high) );
-                    ps.setDouble(4, KelvinToFahrenheit(low) );
-                    ps.setString( 5, dateFormat.format(date) );
-
-                    ps.executeUpdate();
-                    ps.close();
-
-                    conn.commit();
-                    conn.close();
-
-                } catch( IOException e ) {
-                    e.printStackTrace();
-                } catch( JSONException e ) {
-                    e.printStackTrace();
-                } catch( SQLException e ) {
-                    e.printStackTrace();
                 }
             }
         };
 
-        timer.schedule( hourlyTask, 5000, 1000 * 60 * 60 );
+        timer.schedule( hourlyTask, 5000, 1000 * 60 );
     }
 
     private double KelvinToFahrenheit( double temp ) {
